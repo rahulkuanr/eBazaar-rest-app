dde9d952eed69f51268064af4a7c25b824f2f003
rahulkuanr <rahulkuanr@gmail.com>
2023-08-31 01:04:47 +0530

    add user signup

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
+const userRoutes = require("./api/routes/users");
 
 mongoose.connect(
   `mongodb+srv://admin:${process.env.MONGO_ATLAS_PW}@ebazaar-node-rest.pijxpow.mongodb.net/?retryWrites=true&w=majority`
@@ -32,6 +33,7 @@ app.use((req, res, next) => {
 
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
+app.use("/users", userRoutes);
 
 app.use((req, res, next) => {
   const error = new Error("Not Found");
